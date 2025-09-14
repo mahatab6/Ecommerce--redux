e9cdde93e9bec5219f335c57b28ff9bc9e7f402f
mahatav6 <mahatabcall56@gmail.com>
2025-09-15 01:16:52 +0600

    findIndex handle

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,72 +1,82 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   products: [],
   isLoading: false,
-  error: null
-}
+  error: null,
+};
 
-export const fetchProducts = createAsyncThunk ('products/fetchProducts', 
-    async () => {
-        const res = await axios.get('http://localhost:3005/products');
-        return (res.data)
-    }
-)
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    const res = await axios.get("http://localhost:3005/products");
+    return res.data;
+  }
+);
 
-export const deleteProducts = createAsyncThunk ('products/deleteProducts', 
-    async (id) => {
-        const res = await axios.delete(`http://localhost:3005/products/${id}`);
-        return id;
-    }
-)
+export const deleteProducts = createAsyncThunk(
+  "products/deleteProducts",
+  async (id) => {
+    const res = await axios.delete(`http://localhost:3005/products/${id}`);
+    return id;
+  }
+);
 
-export const createProducts = createAsyncThunk ('products/CreateProducts', 
-    async (product) => {
-        const res = await axios.post('http://localhost:3005/products', product);
-        return res.data;
-    }
-)
+export const createProducts = createAsyncThunk(
+  "products/CreateProducts",
+  async (product) => {
+    const res = await axios.post("http://localhost:3005/products", product);
+    return res.data;
+  }
+);
 
-export const updateProducts = createAsyncThunk ('products/updateProducts', 
-    async ({id, product}) => {
-        const res = await axios.put(`http://localhost:3005/products/${id}`, product);
-        console.log(res.data)
-        return res.data;
-    }
-)
+export const updateProducts = createAsyncThunk(
+  "products/updateProducts",
+  async ({ id, product }) => {
+    const res = await axios.put(
+      `http://localhost:3005/products/${id}`,
+      product
+    );
+    console.log(res.data);
+    return res.data;
+  }
+);
 
 export const productsSlice = createSlice({
-  name: 'products',
+  name: "products",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-    .addCase(fetchProducts.pending, (state) =>{
+      .addCase(fetchProducts.pending, (state) => {
         state.isLoading = true;
         state.error = null;
-    })
-    .addCase(fetchProducts.fulfilled, (state, action) => {
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
         state.isLoading = false;
         state.products = action.payload;
-    })
-    .addCase(fetchProducts.rejected, (state, action) => {
-        state.error = "Failed to fetch data" || action.error.message
-    })
-    .addCase(deleteProducts.fulfilled, (state, action) => {
-        state.products = state.products.filter((product) => product.id !== action.payload)
-    })
-    .addCase(createProducts.fulfilled, (state, action) => {
-        state.products.push(action.payload)
-    })
-    .addCase(updateProducts.fulfilled, (state, action) => {
-        const idex = state.products.findIndex(product => product.id === action.payload.id);
-        state.products[idex] = action.payload;
-    })
-  }
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.error = "Failed to fetch data" || action.error.message;
+      })
+      .addCase(deleteProducts.fulfilled, (state, action) => {
+        state.products = state.products.filter(
+          (product) => product.id !== action.payload
+        );
+      })
+      .addCase(createProducts.fulfilled, (state, action) => {
+        state.products.push(action.payload);
+      })
+      .addCase(updateProducts.fulfilled, (state, action) => {
+        const index = state.products.findIndex(
+          (product) => product.id === action.payload.id
+        );
+        if (index !== -1) {
+          state.products[index] = action.payload;
+        }
+      });
+  },
+});
 
-})
-
-
-
-export default productsSlice.reducer
+export default productsSlice.reducer;
